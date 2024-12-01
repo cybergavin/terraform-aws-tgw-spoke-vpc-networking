5e9b4d254e5a3504ee1b82a114a910f06eaac7a2
Gavin Satur <cybergavin@gmail.com>
2024-11-30 23:08:48 -0800

    Add example

---
+++ examples/01-tgw-spoke-vpc/main.tf
@@ -0,0 +1,17 @@
+# Sales Account Networking
+module "sales-network" {
+  source                     = "cybergavin/tgw-spoke-vpc-networking/aws"
+  version                    = "2.0.0"
+  org                        = var.org
+  app_id                     = var.app_id
+  environment                = var.environment
+  vpc_cidr                   = var.vpc_cidr
+  tgw_sharing_enabled        = var.tgw_sharing_enabled
+  shared_transit_gateway_arn = var.shared_transit_gateway_arn
+  transit_gateway_id         = var.transit_gateway_id
+  subnet_cidrs               = var.subnet_cidrs
+  dns_servers                = var.dns_servers
+  dns_domain                 = var.dns_domain
+  security_groups            = var.security_groups
+  global_tags                = var.global_tags
+}
